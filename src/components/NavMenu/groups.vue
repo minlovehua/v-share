62237f52813e6dc51e0f3d9c0b681fffb8a38b42
司城 <321512939@qq.com>
2020-03-27 00:23:50 +0800

    侧边栏对应组件的界面

--- src/components/NavMenu/groups.vue
+++ src/components/NavMenu/groups.vue
@@ -1,17 +1,66 @@
 <template>
-    <div>
-        <h3>
-            这是 团队
-        </h3>
+    <!-- 团队界面 -->
+    <div class="tableBox">
+        <div class="title">我参与的团队<el-button type="primary" size="small">新建团队</el-button></div>
+        <el-table
+        :data="tableData"
+        style="width: 100%"
+        fit>
+        <el-table-column
+            prop="name"
+            label="名称">
+        </el-table-column>
+        <el-table-column
+            prop="introduction"
+            label="简介">
+        </el-table-column>
+        <el-table-column
+            prop="member"
+            label="成员">
+        </el-table-column>
+        </el-table>
     </div>
+
 </template>
 
 <script>
-export default {
-    
-}
+    export default {
+      data() {
+        return {
+          tableData: [{
+            name:'Locus',
+            introduction:'这是一个有爱的强大的团队',
+            member:'13人'
+          }]
+        }
+      }
+    }
 </script>
 
 <style lang="scss" scoped>
+    //给table弄一个浅灰色外边框
+    .tableBox{  
+        border: 1px solid rgb(214, 214, 214);
+        width: 70%;
+    }
 
+    //标题
+    .title{
+      width: 100%;
+      height: 50px;
+      padding: 10px;
+      text-align: left;
+      background-color: snow;
+      box-sizing: border-box;
+      border-bottom: 1px solid #eaeaea;
+      position: relative;
+    }
+
+    //'新建团队'按钮
+    .el-button{
+      position: absolute;
+      right: 10px;
+    }
 </style>
+
+
--- src/components/NavMenu/storehouse.vue
+++ src/components/NavMenu/storehouse.vue
@@ -1,17 +1,81 @@
 <template>
-    <div>
-        <h3>
-            这是 知识库
-        </h3>
+    <!-- 文档界面 -->
+    <div class="tableBox">
+        <div class="title">知识库<el-button type="primary" size="small">新建知识库</el-button></div>
+        <el-table
+        :data="tableData"
+        style="width: 100%"
+        fit>
+        <el-table-column
+            prop="name"
+            label="名称">
+        </el-table-column>
+        <el-table-column
+            prop="whose"
+            label="归属">
+        </el-table-column>
+        <el-table-column
+            prop="introduction"
+            label="简介">
+        </el-table-column>
+        <el-table-column
+            prop="edit"
+            label="操作">
+        </el-table-column>
+        </el-table>
     </div>
+
 </template>
 
 <script>
-export default {
-    
-}
+    export default {
+      data() {
+        return {
+          tableData: [{
+            name:'后端开发',
+            whose:'locus',
+            introduction:'包括C++服务端, java web之类的',
+            edit:'管理'
+          }, {
+            name:'信安开发',
+            whose:'locus',
+            introduction:'学习信息安全的建议',
+            edit:'管理'
+          },{
+            name:'算法',
+            whose:'locus',
+            introduction:'包括数据结构, 算法分析',
+            edit:'管理'
+          }]
+        }
+      }
+    }
 </script>
 
 <style lang="scss" scoped>
+    //给table弄一个浅灰色外边框
+    .tableBox{  
+        border: 1px solid rgb(214, 214, 214);
+        width: 90%;
+    }
 
+    //标题
+    .title{
+      width: 100%;
+      height: 50px;
+      padding: 10px;
+      text-align: left;
+      background-color: snow;
+      box-sizing: border-box;
+      border-bottom: 1px solid #eaeaea;
+      position: relative;
+    }
+
+    //'新建文档'按钮
+    .el-button{
+      position: absolute;
+      right: 10px;
+    }
 </style>
+
+
